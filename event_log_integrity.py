3f5110a2d44722abfc81d772e6866784ce77fbf4
alvaro14s <escalona.alvaro.13@gmail.com>
2024-07-16 19:57:24 -0400

    6 Fix metrics

--- event_log_integrity.py
+++ event_log_integrity.py
@@ -14,14 +14,13 @@ def overlap_metric(df_log: pd.DataFrame) -> pd.DataFrame:
   df_case_lagged['id'] = df_copy['id'].astype(int)
   renamed_columns = {'activity': 'future_activity', 'timestamp': 'future_timestamp', 'timestamp_end': 'future_timestamp_end'}
   df_case_lagged = df_case_lagged.rename(columns=renamed_columns)
-  df_join = pd.merge(df_copy, df_case_lagged, on='id').dropna()
+  df_join = pd.merge(df_copy, df_case_lagged, on='id').dropna(subset=['future_timestamp', 'future_timestamp_end'])
   case_total_events = df_copy.shape[0]
-  df_overlapping_events = df_join[df_join['timestamp_end'] > df_join['future_timestamp']][['activity', 'future_activity']]
+  df_overlapping_events = df_join[(df_join['timestamp_end'] > df_join['future_timestamp']) & (df_join['timestamp_end'] < df_join['future_timestamp_end'])][['activity', 'future_activity']]
   overlap_metric_value = df_overlapping_events.shape[0] / case_total_events
   print(f'Overlap Metric: {overlap_metric_value:.2%}')
 
   return df_overlapping_events
-
 
 def events_contained_metric(df_log: pd.DataFrame) -> pd.DataFrame:
   """
@@ -36,9 +35,9 @@ def events_contained_metric(df_log: pd.DataFrame) -> pd.DataFrame:
   df_case_lagged['id'] = df_copy['id'].astype(int)
   renamed_columns = {'activity': 'future_activity', 'timestamp': 'future_timestamp', 'timestamp_end': 'future_timestamp_end'}
   df_case_lagged = df_case_lagged.rename(columns=renamed_columns)
-  df_join = pd.merge(df_copy, df_case_lagged, on='id').dropna()
-  df_contained_events = df_join[(df_join['timestamp'] <= df_join['future_timestamp']) &\
-                                (df_join['timestamp_end'] >= df_join['future_timestamp_end'])
+  df_join = pd.merge(df_copy, df_case_lagged, on='id').dropna(subset=['timestamp', 'timestamp_end', 'future_timestamp'])
+  df_contained_events = df_join[(df_join['timestamp'] < df_join['future_timestamp']) &\
+                                (df_join['timestamp_end'] > df_join['future_timestamp_end'])
                                ]
   contained_events = df_contained_events.shape[0]
   events_contained_metric_value = contained_events / df_join.shape[0]
